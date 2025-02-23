071249fd02f57bce66a33056937d4a4645893334
Riya <149504570+Riyakhilari@users.noreply.github.com>
2025-02-23 20:57:20 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const App = () => {
-  return (
-    <div id="main"></div>
-  )
+const MyComponent = () => {
+  return <p>I am learning React. My life is getting better.</p>;
+};
+
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(<MyComponent />);
+} else {
+  console.error("No root element found in the document.");
 }
-
-
-export default App;
